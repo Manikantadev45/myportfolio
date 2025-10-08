0f226a1b60403799f2ec69b94af8bfc2c22d406e
Manikantadev45 <manikantadevg@gmail.com>
2025-10-08 15:50:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 
+document.getElementById("hotelMenu").onclick = function() {
+    window.location.href = "https://manikantadev45.github.io/Hotels/","_blank";
+  };
+
+  const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
+
+// Highlight nav link on scroll
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove("active");
+                document.querySelector('header nav a[href*=' + id + ']').classList.add("active");
+            });
+        }
+    });
+};
+ document.addEventListener("keydown", function (e) {
+     if (
+         e.key === "F12" || 
+         (e.ctrlKey && e.shiftKey && e.key === "I") || 
+         (e.ctrlKey && e.shiftKey && e.key === "J") || 
+         (e.ctrlKey && e.key === "U")
+     ) {
+         e.preventDefault();
+     }
+ });
+ document.addEventListener("contextmenu", function (e) {
+     e.preventDefault();
+ });
 document.getElementById("whatsappLink").addEventListener("click", function (e) {
     e.preventDefault();
     const phoneNumber = "917013063222"; 
@@ -28,4 +65,5 @@ document.getElementById("whatsappLink").addEventListener("click", function (e) {
       console.log("FAILED...", error);
     });
   });
+
 
